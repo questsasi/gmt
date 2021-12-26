1dfe88d603d0c6bca5e90a3f58c0acb8a31f5771
sasikumar <questsasi@gmail.com>
2021-12-26 18:40:45 +0530

    Dashboard added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+import { AppService } from 'src/app/app.service';
+import { DataSharedService } from 'src/app/common/data-shared.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
-  constructor() { }
+  lineDetails: any = [];
+  selectedDate: any;
+  flags = {
+    displayLoader: false,
+  };
+  report: any = {};
+  private serviceSubscription: Subscription = new Subscription;
+
+  constructor(
+    private appService: AppService,
+    private dataSharedService: DataSharedService,
+    private title: Title) {
+    this.title.setTitle("GMT PRO - Factory Report");
+  }
+
+  ngOnInit(): void {
+    this.serviceSubscription = this.dataSharedService.getDate().subscribe((getDate: any) => {
+      this.selectedDate = getDate;
+      this.getReports();
+    });
+  }
+
+  getReports() {
+    this.flags.displayLoader = true;
+    this.appService.getFactoryReports(
+      this.selectedDate,
+      (response: any) => {
+        if (response.data) {
+          this.report = response.data;
+        }
+        this.flags.displayLoader = false;
+      },
+      (error: any) => {
+        console.error('Error in fetching factory report', error);
+        this.flags.displayLoader = false;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.serviceSubscription.unsubscribe();
+  }
 }
