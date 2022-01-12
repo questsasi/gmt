105fe2d1b8bd9877a8bbeab7b0b924c931238d15
sasikumar <questsasi@gmail.com>
2022-01-12 23:23:31 +0530

    Footer scroll update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,13 +10,14 @@ export class FooterComponent {
   currentDateTime: Date = new Date();
 
   scrollTop() {
-    const elemMainPanel = <HTMLElement>document.querySelector(".main-panel");
-    elemMainPanel.scroll({ top: 0, behavior: 'smooth' });
-    const ps = new PerfectScrollbar(elemMainPanel);
-    ps.update();
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
+    const elemMainPanel = <HTMLElement>document.querySelector(".main-panel");
+    elemMainPanel.scroll({ top: 0, behavior: 'smooth' });
+    const ps = new PerfectScrollbar(elemMainPanel);
+    ps.update();
+    
   }
 }
